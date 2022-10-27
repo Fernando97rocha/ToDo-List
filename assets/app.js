22fb6eeac7ce7707cffb7a3505aa29bb8c0875b2
unknown <nandocf20@gmail.com>
2022-10-27 13:34:23 -0300

    implementação de botão de editar

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@ const inputSubmit = document.querySelector('.submit')
 inputSubmit.addEventListener('click', () => {
     if (inputText.value !== '') {
         const li = document.createElement('li')
-	    li.innerHTML = `<span class="task-span"><input type="checkbox" class="checkbox">${inputText.value}</span><button>x</button>`
+	    li.innerHTML = `<span class="task-span"><input type="checkbox" class="checkbox">${inputText.value}</span> <button class="edit-button">Editar</button><button class="delete-button">x</button>`
 
 	    li.classList.add('item', 'list')
 	    ul.prepend(li)
@@ -16,9 +16,19 @@ inputSubmit.addEventListener('click', () => {
 
 ul.addEventListener('click', event => {
     const clickedElement = event.target
+    const parentElement = clickedElement.parentElement
+    const firstChildParentElement = parentElement.firstChild
+    
+    //botão de apagar
+    if (clickedElement.className === 'delete-button') {
+        parentElement.remove()        
+    }
 
-    if (clickedElement.tagName === 'BUTTON') {
-        clickedElement.parentElement.remove()        
+    //botão de editar
+    if (clickedElement.className === 'edit-button') {
+        let valueToEdit = firstChildParentElement.textContent
+        inputText.value = valueToEdit
+        parentElement.remove()
     }
 })
 
